57789da85298e24f5865873f83d65bab4ca4d81b
TheKatarGit <d.mihalkovich@gmail.com>
2017-04-22 23:49:15 +0300

    Added preimagined structure of application

---
+++ builder.py
@@ -0,0 +1,5 @@
+class Builder(object):
+    """docstring for Builder."""
+    def __init__(self, arg):
+        super(Builder, self).__init__()
+        self.arg = arg
---
+++ puller.py
@@ -0,0 +1,5 @@
+class Puller(object):
+    """docstring for Puller."""
+    def __init__(self, arg):
+        super(Puller, self).__init__()
+        self.arg = arg
